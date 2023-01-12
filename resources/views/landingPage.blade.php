cc71f173cb3d03b8837a99f3915a842843f0be3b
akramulfata10 <superlay582@gmail.com>
2023-01-12 17:37:25 +0700

    membuat landing page di bagian navbar

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <title>Website Belajar Online</title>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary shadow-sm p-3 mb-5 bg-body rounded-3 sticky-lg-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Online Course</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto ps-5 mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Course</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto ps-5 mb-2 mb-lg-0 me-4">
+                    <button type="submit" class="btn btn-primary mt-2 me-2">Masuk</button>
+                    <button type="submit" class="btn btn-primary mt-2 me-2">Daftar</button>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid jumbotron">
+        <div class="row">
+            <div class="col-lg-6 ">
+                lsmldfmlsdm
+            </div>
+            <div class="col-lg-6">
+                lsmldfmlsdm
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
