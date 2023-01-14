c6fca34b12841d255a0eaa2e93d645d12f858035
akramulfata10 <superlay582@gmail.com>
2023-01-14 23:55:40 +0700

    coba

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -2,12 +2,8 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -27,7 +23,7 @@
 </head>
 
 <body>
-    <header id="header" class="fixed-top ">
+    <header id="header" class="fixed-top">
         <div class="container d-flex align-items-center">
 
             <h1 class="logo me-auto"><a href="/">Online Course</a></h1>
