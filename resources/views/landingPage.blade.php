12b2a292ca863c20cb367a9be7b2cf611bbcccee
akramulfata10 <superlay582@gmail.com>
2023-01-14 23:37:55 +0700

    hapus komentar

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -13,17 +13,9 @@
 
     <meta content="" name="description">
     <meta content="" name="keywords">
-
-    <!-- Favicons -->
-    {{-- <link href="assets/img/favicon.png" rel="icon"> --}}
-    {{-- <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> --}}
-
-    <!-- Google Fonts -->
     <link
         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
         rel="stylesheet">
-
-    <!-- Vendor CSS Files -->
     <link href="{{ asset('') }}assets/vendor/aos/aos.css" rel="stylesheet">
     <link href="{{ asset('') }}assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('') }}assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -31,17 +23,10 @@
     <link href="{{ asset('') }}assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="{{ asset('') }}assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="{{ asset('') }}assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
     <link href="{{ asset('') }}assets/css/style.css" rel="stylesheet">
-
-    {{-- https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ --}}
-
 </head>
 
 <body>
-
-    <!-- ======= Header ======= -->
     <header id="header" class="fixed-top ">
         <div class="container d-flex align-items-center">
 
@@ -63,8 +48,6 @@
 
         </div>
     </header>
-
-    <!-- ======= Hero Section ======= -->
     <section id="hero" class="d-flex align-items-center">
         <div class="container">
             <div class="row">
@@ -85,10 +68,8 @@
             </div>
         </div>
 
-    </section><!-- End Hero -->
-
+    </section>
     <main id="main">
-        <!-- ======= Clients Section ======= -->
         <section id="services" class="services section-bg">
             <div class="container" data-aos="fade-up">
 
@@ -140,8 +121,7 @@
                 </div>
 
             </div>
-        </section><!-- End Services Section -->
-        <!-- ======= Portfolio Section ======= -->
+        </section>
         <section id="course" class="course">
             <div class="container" data-aos="fade-up">
                 <div class="section-title">
@@ -174,9 +154,7 @@
                     </div>
                 </div>
             </div>
-        </section><!-- End Portfolio Section -->
-
-        <!-- ======= Team Section ======= -->
+        </section>
         <section id="team" class="team section-bg">
             <div class="container" data-aos="fade-up">
 
@@ -233,9 +211,7 @@
                 </div>
 
             </div>
-        </section><!-- End Team Section -->
-
-        <!-- ======= Contact Section ======= -->
+        </section>
         <section id="contact" class="contact">
             <div class="container" data-aos="fade-up">
 
@@ -359,20 +335,13 @@
                 &copy; Copyright <strong><span>Akramul Fata </span></strong>. All Rights Reserved
             </div>
             <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
                 Designed by <a href="https://github.com/akramulfata10">BootstrapMade</a>
             </div>
         </div>
-    </footer><!-- End Footer -->
-
+    </footer>
     <div id="preloader"></div>
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
-
-    <!-- Vendor JS Files -->
     <script src="{{ asset('') }}assets/vendor/aos/aos.js"></script>
     <script src="{{ asset('') }}assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('') }}assets/vendor/glightbox/js/glightbox.min.js"></script>
@@ -380,10 +349,7 @@
     <script src="{{ asset('') }}assets/vendor/swiper/swiper-bundle.min.js"></script>
     <script src="{{ asset('') }}assets/vendor/waypoints/noframework.waypoints.js"></script>
     <script src="{{ asset('') }}assets/vendor/php-email-form/validate.js"></script>
-
-    <!-- Template Main JS File -->
     <script src="{{ asset('') }}assets/js/main.js"></script>
-
 </body>
 
 </html>
